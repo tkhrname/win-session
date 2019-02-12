b8e509521356a60e9a83d8825fd2ff100d60e569
渡邊丈洋 <takehiro@watanabejouyounomacbook-air.local>
2019-02-12 22:44:34 +0900

    create circle

--- win-session/DrawCircleView.swift
+++ win-session/DrawCircleView.swift
@@ -23,18 +23,20 @@ class DrawCiercleView: UIView {
     override func draw(_ rect: CGRect) {
         // 円
         let circle: UIBezierPath = UIBezierPath(
-            arcCenter: CGPoint(x: 0, y: 0),
+            arcCenter: CGPoint(x: rect.origin.x, y: rect.origin.y),
             radius: 100,
             startAngle: 0,
-            endAngle: CGFloat(Double.pi),
+            endAngle: .pi * 2,
             clockwise: true
         )
         // 内側の色
         UIColor(red: 0, green: 0, blue: 1, alpha: 0.3).setFill()
         // 内側を塗りつぶす
         circle.fill()
-        
-        
+        // 線の色
+        UIColor(red: 0, green: 0, blue: 1, alpha: 1.0).setStroke()
+        // 線の太さ
+        circle.lineWidth = 2.0
         // 線を塗りつぶす
         circle.stroke()
     }
--- win-session/ViewController.swift
+++ win-session/ViewController.swift
@@ -13,8 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let circle = DrawCiercleView(frame: self.view.bounds)
-        self.view.addSubview(circle)
+        let grayPath = UIBezierPath()
+        print(Double.pi * 2)
+        grayPath.addArc(
+            withCenter: CGPoint(x: self.view.frame.width/2, y: self.view.frame.height / 2), // 中心
+            radius: 100, // 半径
+            startAngle: 0, // 開始角度
+//            endAngle: .pi * 2.0, // 終了角度
+            endAngle: 6.283, // 終了角度
+            clockwise: true) // 時計回り
+        
+        let grayLayer = CAShapeLayer()
+        grayLayer.path = grayPath.cgPath
+        grayLayer.fillColor = UIColor.clear.cgColor // 塗り色
+        grayLayer.strokeColor = UIColor(red: 0.80, green: 0.80, blue: 0.80, alpha: 1.0).cgColor // 線の色
+        grayLayer.lineWidth = 20.0 // 線の幅
+        self.view.layer.addSublayer(grayLayer)
     }
 
 
